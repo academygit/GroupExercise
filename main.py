2c63f1ee8f10c7e8dd479a3bd0e58d85856ebf84
hanezki <hanezki.manninen@gmail.com>
2021-10-15 15:19:27 +0300

    pelaa funktio tehty

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
+kysymykset =[
+"Mistä gcp on lyhenne? \nA: Google Computing Platform \nB: Google Cloud Platform \nC: Google Customer Platform",
+"Mistä tietää millä käyttäjällä operoit? \nA: whoami \nB: whereami \nC: whenami",
+"Mikä näistä virtuaalikonetyypeistä soveltuu parhaiten intensiivisiin prosessointitöihin? \nA: M2 \nB: E2 \nC: C2"
+]
+maksimi_pisteet = len(kysymykset)
 #tulostaa ohjeet kysymyspelin pelaamiseen
 def ohje():
     pass
+
 
 #tulostaa kysymykset
 #ja laskee oikeat vastaukset
 #ja tulostaa pisteet
 def pelaa():
-    pass
+    i = 0
+    pisteet = 0
+    for kysymys in kysymykset:
+        try:
+            vastaus = str(input(f"\n{kysymys}\n>"))
+        except ValueError:
+            print("Virheellinen komento")
+        if i == 0:
+            if vastaus.lower() == "b":
+                print("Oikein! Sait yhden pisteen")
+                pisteet += 1
+            else:
+                print("Väärin!")
+        elif i == 1:
+            if vastaus.lower() == "a":
+                print("Oikein! Sait yhden pisteen")
+                pisteet += 1
+            else:
+                print("Väärin!")
+        elif i == 2:
+            if vastaus.lower() == "c":
+                print("Oikein! Sait yhden pisteen")
+                pisteet += 1
+            else:
+                print("Väärin!")
+        i += 1
+    print(f"Sait {pisteet}/{maksimi_pisteet} pistettä!")
 
 #while-loop joka kutsuu kysymyksentulostusta, käsittelee pelaajan syötteen
 #1 pelaa 0 lopeta
@@ -14,3 +47,5 @@ def suorita():
     pass
 
 suorita()
+
+pelaa()
