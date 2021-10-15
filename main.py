f1f872718f289075a2602b0ab8fd850741c30601
jajuhir <jhirvioja@gmail.com>
2021-10-15 15:07:57 +0300

    Suorita-metodi

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 #tulostaa ohjeet kysymyspelin pelaamiseen
 def ohje():
-    pass
+    return print("Ohje tulee tähän")
 
 #tulostaa kysymykset
 #ja laskee oikeat vastaukset
 #ja tulostaa pisteet
 def pelaa():
-    pass
+    return print("Peli pyörii")
 
 #while-loop joka kutsuu kysymyksentulostusta, käsittelee pelaajan syötteen
 #1 pelaa 0 lopeta
 def suorita():
-    pass
+    print(ohje)
+    kysymys = int(input("Oletko valmis pelaamaan? 1 = Pelaa, 0 = Lopeta "))
+    if kysymys == 1:
+        return pelaa()
+    elif kysymys == 0:
+        pass
 
 suorita()
 
-#testikommentti
+print("Kiitos pelaamisesta!")
