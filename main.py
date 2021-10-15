b3ed148342330dd49a2a6f7fe5c5f3162517f5e0
jajuhir <jhirvioja@gmail.com>
2021-10-15 14:43:59 +0300

    Testi

---
+++ main.py
@@ -0,0 +1,18 @@
+#tulostaa ohjeet kysymyspelin pelaamiseen
+def ohje():
+    pass
+
+#tulostaa kysymykset
+#ja laskee oikeat vastaukset
+#ja tulostaa pisteet
+def pelaa():
+    pass
+
+#while-loop joka kutsuu kysymyksentulostusta, käsittelee pelaajan syötteen
+#1 pelaa 0 lopeta
+def suorita():
+    pass
+
+suorita()
+
+#testikommentti
